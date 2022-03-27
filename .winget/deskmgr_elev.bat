42fe2eb9ade7ffad093606f1013cdeeeec8f4304
Jupiter <71598437+DeveloperWOW64@users.noreply.github.com>
2022-03-27 18:11:47 +0100

    Remove old files

--- .winget/deskmgr_elev.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo For most people, just hit enter when Windows prompts you to type in the Admin password.
-runas.exe /savecred /user:administrator "%temp%\deskmgr_ins.bat"
--- .winget/ins.old.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo If Windows shows you a prompt for admin, or related, accept it.
-bitsadmin.exe /transfer "DesktopManagement" https://github.com/DeveloperWOW64/deskmgr/releases/download/v1.2.3/deskmgr.msc C:\Windows\System32\deskmgr.msc
-cd C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\
-mklink /D Desktop Management C:\Windows\System32\deskmgr.msc
-cd %windir%\System32
-echo It should be done by now, please check by typing 'Desktop Management' into the start menu.
